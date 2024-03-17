2ddafb0e4598e80d85bd7a8a503c08e68efe3ed0
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2024-03-17 19:55:51 +0100

    update description of lambda function helpers, add attribution

---
+++ Source/UnNetHost/Private/LambdaHelpers.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+/**
+ * Helper structures and functions to allow conversion of lambda with captures to a function pointer.
+ * Based on Evgeny Karpov's reply to this post: https://stackoverflow.com/questions/7852101/c-lambda-with-captures-as-a-function-pointer
+ */
+namespace LambdaSnail::UnrealSharp::Lambda
+{
+	template <class TClass>
+	struct TLambdaTraits : TLambdaTraits<decltype(&TClass::operator())>
+	{
+	};
+
+	template <typename TLambda, typename TReturn, typename... TArgs>
+	struct TLambdaTraits<TReturn(TLambda::*)(TArgs...)> : TLambdaTraits<TReturn(TLambda::*)(TArgs...) const>
+	{
+	};
+
+	template <class TLambda, class TReturn, class... TArgs>
+	struct TLambdaTraits<TReturn(TLambda::*)(TArgs...) const>
+	{
+		using Pointer = std::add_pointer_t<TReturn(TArgs...)>;
+
+		static Pointer const ToFunctionPointer(TLambda&& InFunction)
+		{
+			static TLambda Function = std::forward<TLambda>(InFunction);
+			return [](TArgs... Args)
+			{
+				return Function(std::forward<TArgs>(Args)...);
+			};
+		}
+	};
+
+	template <class TLambda>
+	typename TLambdaTraits<TLambda>::Pointer ToFunctionPointer(TLambda&& Function)
+	{
+		return TLambdaTraits<TLambda>::ToFunctionPointer(std::forward<TLambda>(Function));
+	}
+}
--- Source/UnNetHost/Private/UnrealSharpSubsystem.cpp
+++ Source/UnNetHost/Private/UnrealSharpSubsystem.cpp
@@ -6,64 +6,39 @@
 #include <minwindef.h>
 
 #include "Host.h"
+#include "LambdaHelpers.h"
 #include "Logging/StructuredLog.h"
-
-
-template <class F>
-struct lambda_traits : lambda_traits<decltype(&F::operator())>
-{ };
-
-template <typename F, typename R, typename... Args>
-struct lambda_traits<R(F::*)(Args...)> : lambda_traits<R(F::*)(Args...) const>
-{ };
-
-template <class F, class R, class... Args>
-struct lambda_traits<R(F::*)(Args...) const> {
-	using pointer = typename std::add_pointer<R(Args...)>::type;
-
-	static pointer const cify(F&& f) {
-		static F fn = std::forward<F>(f);
-		return [](Args... args) {
-			return fn(std::forward<Args>(args)...);
-		};
-	}
-};
-
-template <class F>
-inline typename lambda_traits<F>::pointer cify(F&& f) {
-	return lambda_traits<F>::cify(std::forward<F>(f));
-}
 
 
 void UUnrealSharpSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
 	using namespace LambdaSnail::UnrealSharp;
-	
+
 	const wchar_t* path = L"C:\\Projects\\Unreal\\DotnetIntegration\\Plugins\\UnNetHost\\Resources\\";
 	ActorFunctions = UnNet_Execute(path);
 }
 
 void UUnrealSharpSubsystem::Deinitialize()
 {
-	
 }
 
 LambdaSnail::UnrealSharp::ActorHandle UUnrealSharpSubsystem::RegisterActorForTick(AActor* Actor) const
 {
 	using namespace LambdaSnail::UnrealSharp;
-	
-	ActorHandle const handle = ActorFunctions.register_managed_actor(STR("UnrealSharpCore"), STR("LambdaSnail.UnrealSharp.TestActor"));
 
-	ActorFunctions.bind_delegates(handle, cify([Actor]()
-	{
-		UE_LOGFMT(LogTemp, Warning, "GetTransform for handle {Handle}", 1);
-		SimpleTransform const Transform { Actor->GetActorLocation() };
-		return Transform;
-	}),
-	cify([Actor](SimpleTransform Transform)
-	{
-		Actor->SetActorLocation(Transform.Location);
-	}
+	ActorHandle const handle = ActorFunctions.register_managed_actor(
+		STR("UnrealSharpCore"), STR("LambdaSnail.UnrealSharp.TestActor"));
+
+	ActorFunctions.bind_delegates(handle, Lambda::ToFunctionPointer([Actor]()
+	    {
+	        UE_LOGFMT(LogTemp, Warning, "GetTransform for handle {Handle}", 1);
+	        SimpleTransform const Transform{Actor->GetActorLocation()};
+	        return Transform;
+	    }),
+Lambda::ToFunctionPointer([Actor](SimpleTransform Transform)
+	    {
+	        Actor->SetActorLocation(Transform.Location);
+	    }
 	));
 
 	return handle;
