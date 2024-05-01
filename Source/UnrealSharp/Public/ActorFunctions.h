42b2733a5dc9a7888e26077ebc51f152ac47adc7
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2024-05-01 21:41:36 +0200

    move hand-coded actor functions to separate header

---
+++ Source/UnrealSharp/Public/ActorFunctions.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+// TODO: Can we find a way to use the FTransform or auto generate this kind of structure?
+// TODO: Struct with single member compiles but crashes at runtime
+struct Transform
+{
+	//FTransform T;
+	FVector3d _R;
+	FVector3d _T;
+	FVector3d _S;
+};
+
+// TODO: Remove lambda based api and use these instead
+// TODO: Return something that is compatible with C
+// TODO: What happens with the ptr when an actor gets destroyed?
+// TODO: Add functions for Translation, Rotation and Scale individually, so that we can set them without
+//	copying stuff that haven't changed
+extern "C" __declspec(dllexport) inline Transform GetTransform(AActor const* Actor)
+{
+	FTransform T = Actor->GetTransform();
+	return { ._R = T.GetRotation().Euler(), ._T = T.GetTranslation(), ._S = T.GetScale3D() };
+}
+
+extern "C" __declspec(dllexport) inline void SetTransform(AActor* Actor, Transform Transform)
+{
+	Actor->SetActorLocation(Transform._T);
+	Actor->SetActorScale3D(Transform._S);
+	Actor->SetActorRotation(FRotator::MakeFromEuler(Transform._R));
+}
--- Source/UnrealSharp/Public/UnrealSharpSubsystem.h
+++ Source/UnrealSharp/Public/UnrealSharpSubsystem.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Host.h"
+#include "ActorFunctions.h"
 #include "LambdaHelpers.h"
 #include "Logging/StructuredLog.h"
 #include "Subsystems/GameInstanceSubsystem.h"
@@ -37,33 +38,6 @@ private:
 	
 	TMap<int, TStrongObjectPtr<AActor>> RegisteredActors;
 };
-
-// TODO: Can we find a way to use the FTransform or auto generate this kind of structure?
-// TODO: Struct with single member compiles but crashes at runtime
-struct Transform
-{
-	//FTransform T;
-	FVector3d _R;
-	FVector3d _T;
-	FVector3d _S;
-};
-
-// TODO: Remove lambda based api and use these instead
-// TODO: Return something that is compatible with C
-// TODO: What happens with the ptr when an actor gets destroyed?
-extern "C" __declspec(dllexport) inline Transform GetTransform(AActor const* Actor)
-{
-	FTransform T = Actor->GetTransform();
-	return { ._R = T.GetRotation().Euler(), ._T = T.GetTranslation(), ._S = T.GetScale3D() };
-}
-
-extern "C" __declspec(dllexport) inline void SetTransform(AActor* Actor, Transform Transform)
-{
-	Actor->SetActorLocation(Transform._T);
-	Actor->SetActorScale3D(Transform._S);
-	Actor->SetActorRotation(FRotator::MakeFromEuler(Transform._R));
-}
-
 
 // TOREAD: https://forums.unrealengine.com/t/correct-way-to-get-the-value-of-a-property/551897/3
 template <typename TProperty>
